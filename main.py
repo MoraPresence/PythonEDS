1b5a35629ac633291ee85989fdeb542731367e82
MoraPresence <od34156la@mail.ru>
2021-02-18 14:47:01 +0300

    version 0.0.3

--- main.py
+++ main.py
@@ -1,127 +1,150 @@
 import random
 from collections import namedtuple
 
-q = 0x8000000000000000000000000000000150FE8A1892976154C59CFC193ACCF5B3
-k = 0
-p = 0x8000000000000000000000000000000000000000000000000000000000000431
-a = 0x7
-b = 0x5FBFF498AA938CE739B8E022FBAFEF40563F6E6A3472FC2A514C0CE9DAE23B7E
-m = 0x8000000000000000000000000000000150FE8A1892976154C59CFC193ACCF5B3
-xP = 0x2
-yP = 0x8E2A8A0E65147D4BD6316030E16D19C85C97F0A9CA267122B96ABBCEA7E8FC8
-
 Point = namedtuple("Point", "x y")
-O = 'Origin'
+O = Point(0, 0)
 
+class ECPoint:
+    myP = 0
+    b = 0x5FBFF498AA938CE739B8E022FBAFEF40563F6E6A3472FC2A514C0CE9DAE23B7E
+    p = 0x8000000000000000000000000000000000000000000000000000000000000431
+    a = 0x7
 
-class ECPoint(Point):
-    def valid(self, P):
-        if P == O:
+    def __init__(self, x, y):
+        self.myP = Point(x, y)
+
+    def _valid(self):
+        if self.myP == O:
             return True
         else:
             return (
-                    (P.y ** 2 - (P.x ** 3 + a * P.x + b)) % p == 0 and
-                    0 <= P.x < p and 0 <= P.y < p)
+                    (self.myP.y ** 2 - (self.myP.x ** 3 + self.a * self.myP.x + self.b)) % self.p == 0 and
+                    0 <= self.myP.x < self.p and 0 <= self.myP.y < self.p)
 
-    def inv_mod_p(self, x):
-        if x % p == 0:
+    def _inv_mod_p(self, x):
+        if x % self.p == 0:
             raise ZeroDivisionError("Impossible inverse")
-        return pow(x, p - 2, p)
+        return pow(x, self.p - 2, self.p)
 
-    def ec_inv(self, P):
-        if P == O:
-            return P
-        return Point(P.x, (-P.y) % p)
+    def _ec_inv(self):
+        if self.myP == O:
+            return self.myP
+        return Point(self.myP.x, (-self.myP.y) % self.p)
 
     def ec_add(self, P, Q):
-        if not (self.valid(P) and self.valid(Q)):
+        if not (P._valid() and Q._valid()):
             raise ValueError("Invalid inputs")
-        if P == O:
+        if P.myP == O:
             result = Q
-        elif Q == O:
+        elif Q.myP == O:
             result = P
-        elif Q == self.ec_inv(P):
+        elif Q.myP == P._ec_inv():
             result = O
         else:
-            if (P.x == Q.x and P.y == Q.y):
-                dydx = (3 * P.x ** 2 + a) * self.inv_mod_p(2 * P.y)
+            if (P.myP.x == Q.myP.x and P.myP.y == Q.myP.y):
+                dydx = (3 * P.myP.x ** 2 + self.a) * self._inv_mod_p(2 * P.myP.y)
             else:
-                dydx = (Q.y - P.y) * self.inv_mod_p(Q.x - P.x)
-            x = (dydx ** 2 - P.x - Q.x) % p
-            y = (dydx * (P.x - x) - P.y) % p
-            result = Point(x, y)
-        assert self.valid(result)
+                dydx = (Q.myP.y - P.myP.y) * self._inv_mod_p(Q.myP.x - P.myP.x)
+            x = (dydx ** 2 - P.myP.x - Q.myP.x) % self.p
+            y = (dydx * (P.myP.x - x) - P.myP.y) % self.p
+            result = ECPoint(x, y)
+        assert result._valid()
         return result
 
-    def bits(self, n):
+    def _bits(n):
         while n:
             yield n & 1
             n >>= 1
 
-    def double_and_add(self, n, x):
-        result = O
+    def double_and_add(n, x):
+        result = ECPoint(0, 0)
         addend = x
 
-        for bit in self.bits(n):
+        for bit in result._bits(n):
             if bit == 1:
-                result = self.ec_add(result, addend)
-            addend = self.ec_add(addend, addend)
+                result = result.ec_add(result, addend)
+            addend = result.ec_add(addend, addend)
         return result
 
+ECPoint.double_and_add = staticmethod(ECPoint.double_and_add)
+ECPoint._bits = staticmethod(ECPoint._bits)
 
-def signature(M, d):
-    h_ = 0
-    e = 0
-    alpha = 3
-    if alpha % q == 0: e = 1
-    e = 0x2DFBC1B372D89A1188C09C52E0EEC61FCE52032AB1022E8E67ECE6672B043EE5
-    s = 0
+class ECP:
+    q = 0
+    m = 0
+    p = 0
+    a = 0
+    b = 0
 
-    while (True):
-        # k = random.uniform(0, q)
-        k = 0x77105C9B20BCD3122823C8CF6FCC7B956DE33814E95B7FE64FED924594DCEAB3
+    def __init__(self, q, m, p, a, b):
+        self.q = q
+        self.m = m
+        self.p = p
+        self.a = a
+        self.b = b
+
+    def signature(self, M, d, P):
+        h_ = 0
+        e = 0
+        alpha = 3
+        if alpha % self.q == 0: e = 1
+        e = 0x2DFBC1B372D89A1188C09C52E0EEC61FCE52032AB1022E8E67ECE6672B043EE5
+        s = 0
+
+        while (True):
+            # k = random.uniform(0, q)
+            k = 0x77105C9B20BCD3122823C8CF6FCC7B956DE33814E95B7FE64FED924594DCEAB3
+
+            C = ECPoint.double_and_add(k, P)
+            r = C.myP.x % self.q
+            if r != 0:
+                s = (r * d + k * e) % self.q
+            if s != 0: break
+
+        return str(r) + str(s)
+
+
+    def verify(self, sign, M, Q):
+        r = int(sign[:77])
+        s = int(sign[77:])
+        if (r <= 0 and r >= self.q and s <= 0 and s >= self.q):
+            return False
+        h_ = 0
+        e = 0
+        alpha = 3
+        if alpha % self.q == 0: e = 1
+        e = 0x2DFBC1B372D89A1188C09C52E0EEC61FCE52032AB1022E8E67ECE6672B043EE5
+        v = pow(e, -1, self.q)
+        z1 = s * v % self.q
+        z2 = -r * v % self.q
+
 
         C = ECPoint(0, 0)
-        P = ECPoint(xP, yP)
-        C = C.double_and_add(k, P)
-        r = C.x % q
-        if r != 0:
-            s = (r * d + k * e) % q
-        if s != 0: break
-
-    return str(r) + str(s)
+        C = C.ec_add(ECPoint.double_and_add(z1, P), ECPoint.double_and_add(z2, Q))
+        R = C.myP.x % self.q
+        if R == r:
+            return True
+        else:
+            return False
 
 
-def verify(sign, M, Q):
-    r = int(sign[:77])
-    s = int(sign[77:])
-    if (r <= 0 and r >= q and s <= 0 and s >= q):
-        return False
-    h_ = 0
-    e = 0
-    alpha = 3
-    if alpha % q == 0: e = 1
-    e = 0x2DFBC1B372D89A1188C09C52E0EEC61FCE52032AB1022E8E67ECE6672B043EE5
-    v = pow(e, -1, q)
-    z1 = s * v % q
-    z2 = -r * v % q
+q = 0x8000000000000000000000000000000150FE8A1892976154C59CFC193ACCF5B3
+m = 0x8000000000000000000000000000000150FE8A1892976154C59CFC193ACCF5B3
+p = 0x8000000000000000000000000000000000000000000000000000000000000431
+a = 0x7
+b = 0x5FBFF498AA938CE739B8E022FBAFEF40563F6E6A3472FC2A514C0CE9DAE23B7E
+engine = ECP(q, m, p, a, b)
 
-    P = ECPoint(xP, yP)
-    C = ECPoint(0, 0)
-    C = C.ec_add(C.double_and_add(z1, P), C.double_and_add(z2, Q))
-    R = C.x % q
-    if R == r:
-        return True
-    else:
-        return False
+xP = 0x2
+yP = 0x8E2A8A0E65147D4BD6316030E16D19C85C97F0A9CA267122B96ABBCEA7E8FC8
 
-
+P = ECPoint(xP, yP)
 d = 0x7A929ADE789BB9BE10ED359DD39A72C11B60961F49397EEE1D19CE9891EC3B28
-sign = signature("hi", d)
+sign = engine.signature("hi", d , P)
 print(sign)
 
 xQ = 0x7F2B49E270DB6D90D8595BEC458B50C58585BA1D4E9B788F6689DBD8E56FD80B
 yQ = 0x26F1B489D6701DD185C8413A977B3CBBAF64D1C593D26627DFFB101A87FF77DA
 Q = ECPoint(xQ, yQ)
 
-print(verify(sign, "hi", Q))
+print(engine.verify(sign, "hi", Q))
